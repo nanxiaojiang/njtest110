8b8202e1309dc495c8aa587647c68fdfe067ffc1
nanjiang <nanjiangdayd@163.com>
2021-02-28 22:38:56 +0800

    test第一次提交

---
+++ quickt/src/main/java/com/nanjiang/quickt/QuicktApplication.java
@@ -0,0 +1,13 @@
+package com.nanjiang.quickt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicktApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuicktApplication.class, args);
+    }
+
+}
